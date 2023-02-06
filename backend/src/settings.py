ac03b94f13b1166aefd5fa03566c143635ecfd74
Yunus <gadamurrr@gmail.com>
2023-02-06 14:48:01 +0300

    back basic conf

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -133,3 +133,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_AUTHENTICATION_CLASSES: [
+    # 'rest_framework.authentication.SessionAuthentication',
+    'rest_framework.authentication.BasicAuthentication',
+]
