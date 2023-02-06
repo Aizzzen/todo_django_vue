049ebc96e737842a8150b93d44da82f877c17548
Yunus <gadamurrr@gmail.com>
2023-02-06 23:23:25 +0300

    post_put_delete&complete

--- frontend/src/components/TodoComponent.vue
+++ frontend/src/components/TodoComponent.vue
@@ -3,26 +3,47 @@
     <v-row>
       <v-col cols="12">
         <v-form ref="form" lazy-validation class="text-center">
-          <v-text-field :counter="64" label="Title" required></v-text-field>
+          <v-text-field
+            v-model="newTodo.title"
+            :counter="64"
+            :rules="newTodo.titleRules"
+            label="Title"
+            required
+          ></v-text-field>
 
           <v-textarea
+            v-model="newTodo.description"
             autocomplete="Description"
             label="Description"
           ></v-textarea>
 
-          <v-checkbox label="Is task completed?"></v-checkbox>
+          <v-checkbox
+            v-model="newTodo.is_complete"
+            label="Is task completed?"
+          ></v-checkbox>
 
-          <v-btn color="error" class="mr-4">Clear</v-btn>
+          <v-btn color="error" class="mr-4" @click="reset">Clear</v-btn>
 
-          <v-btn color="success" class="mr-4">Add</v-btn>
+          <v-btn
+            color="success"
+            class="mr-4"
+            :disabled="!newTodo.title"
+            @click="add"
+            >Add</v-btn
+          >
         </v-form>
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12">
-        <v-list shaped>
+      <v-col cols="12" v-if="todoList.length">
+        <h3>List:</h3>
+        <v-list>
           <v-list-item-group color="deep-purple" v-model="selected" multiple>
-            <v-list-item v-for="item in todoList" :key="item.id">
+            <v-list-item
+              v-for="item in todoList"
+              :key="item.id"
+              @click="updateStatus(item)"
+            >
               <v-list-item-icon>
                 {{ item.id }}
               </v-list-item-icon>
@@ -53,6 +74,16 @@ import axios from "axios";
 export default {
   name: "TodoComponent",
   data: () => ({
+    newTodo: {
+      title: "",
+      description: "",
+      titleRules: [
+        (v) => !!v || "Title is required",
+        (v) => (v && v.length <= 64) || "Title must ne less than 64 characters",
+      ],
+      is_complete: false,
+      user: 1,
+    },
     selected: [],
     todoList: [],
   }),
@@ -62,8 +93,33 @@ export default {
   methods: {
     getTodos() {
       axios
-        .get("http://127.0.0.1:8000/api/v1/todo/")
-        .then((res) => (this.todoList = res.data));
+        .get("http://localhost:8000/api/v1/todo/?ordering=is_complete")
+        .then((res) => {
+          this.todoList = res.data;
+          res.data.forEach((el, i) => {
+            if (!el.is_complete) this.selected.push(i);
+          });
+        });
+    },
+    reset() {
+      this.$refs.form.rest();
+    },
+    add() {
+      let data = this.newTodo;
+      axios.post("http://localhost:8000/api/v1/todo/", data).then((res) => {
+        console.log(res);
+        this.getTodos();
+      });
+    },
+    updateStatus(item) {
+      item.is_complete = !item.is_complete;
+      let data = { is_complete: item.is_complete };
+      axios
+        .patch(`http://localhost:8000/api/v1/todo/${item.id}/`, data)
+        .then((res) => {
+          console.log(res);
+          this.getTodos();
+        });
     },
   },
 };
